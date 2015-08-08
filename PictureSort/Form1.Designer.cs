269316d3954a7bd287414cd80b20393c1e616fce
Mikhaylovskiy M <wizmax@yandex.ru>
2015-08-08 21:35:24 +0300

    First commit

---
+++ PictureSort/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PictureSort
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(2, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(204, 67);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Начать сортировку";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// backgroundWorker1
+			// 
+			this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(2, 88);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(445, 21);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(2, 127);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(445, 21);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(-1, 72);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(149, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Папка с изображениями";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(-1, 111);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(494, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Папка для сортировки (НЕДОЛЖНА НАХОДИТЬСЯ В  СОРТИРУЕМОЙ ДИРЕКТОРИИ)";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(7, 19);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(97, 17);
+			this.radioButton1.TabIndex = 6;
+			this.radioButton1.Text = "Перемещать";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Checked = true;
+			this.radioButton2.Location = new System.Drawing.Point(7, 38);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(94, 17);
+			this.radioButton2.TabIndex = 7;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Копировать";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Location = new System.Drawing.Point(386, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(126, 67);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Сортировать";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(213, 12);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(162, 17);
+			this.checkBox1.TabIndex = 9;
+			this.checkBox1.Text = "Сортировать подпапки";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(453, 125);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(59, 22);
+			this.button2.TabIndex = 11;
+			this.button2.Text = "Обзор";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(453, 86);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(59, 22);
+			this.button3.TabIndex = 12;
+			this.button3.Text = "Обзор";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// folderBrowserDialog1
+			// 
+			this.folderBrowserDialog1.Description = "Выберите папку";
+			this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(2, 153);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(510, 52);
+			this.textBox3.TabIndex = 13;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(518, 210);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "PictureSizeSort";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.TextBox textBox3;
+	}
+}
+
---
+++ PictureSort/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace PictureSort
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		// адреса папок
+		//папка для сортировки
+		string mainFL;
+		//папка с изображениями
+		string localFolder;
+		//имя папки(800х600, 1024х768...)
+		string fldTOCP;
+
+		Stopwatch st;
+		TimeSpan ts;
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//выкл. запрет на доступ к элементам созданым в разных потоках
+			CheckForIllegalCrossThreadCalls = false;
+			st = Stopwatch.StartNew();
+			textBox3.Text = "Идет обработка...";
+			// присваиваем адреса папок
+			if (textBox1.Text.EndsWith(@"\") && textBox2.Text.EndsWith(@"\"))
+			{
+				localFolder = textBox1.Text;
+				mainFL = textBox2.Text;
+			}
+			else
+			{
+				textBox1.Text += @"\";
+				textBox2.Text += @"\";
+				localFolder = textBox1.Text;
+				mainFL = textBox2.Text;
+			}
+			//очистка лога
+			textBox3.Text = "";
+			//в случае если адреса папок не введены
+			if (textBox1.Text == "" || textBox2.Text == "")
+			{
+				MessageBox.Show("Неправильно заданы адреса папок");
+			}
+			else
+			{
+				checkBox1.Enabled = false;
+				backgroundWorker1.RunWorkerAsync();
+			}
+
+		}
+
+		//функция соритировки
+		public int sortIMG(string path)
+		{
+			//имя конечного файла
+			string endFile = "";
+			//имя папки (разрешение)
+			fldTOCP = "";
+			//backup имени файла( на тот случай если даже изменив имя при совпадении, все равно появляется колизия
+			string BCendFile = "";
+
+			try
+			{		//если переданый в функцию файл изображение		
+				if (path.EndsWith(".jpg") || path.EndsWith(".bmp") || path.EndsWith(".png"))
+				{
+					//создаем изображение
+					Bitmap bm = new Bitmap(path);
+					//получаем его размеры
+					fldTOCP = bm.PhysicalDimension.Width.ToString() + "x" + bm.PhysicalDimension.Height.ToString();
+					//уничтожаем что бы освободить файл
+					bm.Dispose();
+					//создаем директорию с разрешением
+					Directory.CreateDirectory(mainFL + fldTOCP);
+					//генерируем путь куда скопировать его
+					//дир. для сортировки + дир. с его разрешением + имя файла
+					endFile = mainFL + fldTOCP  + path.Substring(path.LastIndexOf(@"\"));
+					//отвечает за повторное прохождение цикла в случае коллизии
+					bool fileexist = true;
+					//счетчик в случае коллизии, помогает создать уникальное имя
+					int i = 0;
+					//делаем backup пути куда копировать( на случай коллизии)
+					BCendFile = endFile;
+					while (fileexist)
+					{
+						//получаем информацию о файлу
+						FileInfo infENDFILE = new FileInfo(endFile);
+						//если его не существует, тогда копируем/перемещаем
+						if (!infENDFILE.Exists)
+						{
+							//перемещаем
+							if (radioButton1.Checked == true)
+							{
+								File.Move(path, endFile);
+								//в цикл больше не возвращаемся
+								fileexist = false;
+							} 
+							//копируем
+							if(radioButton2.Checked == true)
+							{
+								File.Copy(path, endFile);
+								//в цикл больше не возвращаемся
+								fileexist = false;
+							}
+	
+						}
+						else
+						{
+							//если возникли коллизия(такой файл уже существует)
+							//колдунство с именем файла если
+							//таковой уже есть в конечной директории
+							//если уже не первый раз заходим в цикл, возвращаем переменной endFile, начальное значение
+							endFile = BCendFile;
+							//копируем переменную endFile, для обработки и присваение ей уник. умени
+							string tmp = endFile;
+							//вырезаем расширение файла
+							string format = tmp.Substring(tmp.LastIndexOf("."));
+							//удалеяем расширение
+							tmp = tmp.Remove(tmp.LastIndexOf("."),4);
+							//дописываем шаг цикла( генерация уникальноги имени)
+							tmp = tmp + "_" + i.ToString();
+							//дописываем расширение
+							tmp = tmp + format;
+							//получаем имя вида ...xxx_1.jpg или xxx_2.jpg
+							endFile = tmp ;
+							//снова проходим цикл для копирования или перемещения файла
+							fileexist = true;
+							//
+							//в случае если снова такой файл уже существует, тогда снова проходим цикл, но в начале 
+							//мы придаем переменной endFile, то значение которое было в самом начале, но т.к. i увеличилось,
+							//имя будет уже другим.
+							//
+
+						
+						}
+						//увеличиваем счетчик при каждом проходу цикла
+						i++;
+					}				
+				}
+			}
+			catch (System.Exception ex)
+			{
+                textBox3.Text += ex.Message; //MessageBox.Show(ex.Message);
+			}
+			
+			return 0;
+		}
+
+		//функция которая проходит по директориям и отдает на обработку все найденые файлы
+		public int GetDirectory(string dr)
+		{
+
+			//получаем список директорий, в переданой нам директории
+			string[] DIR = Directory.GetDirectories(dr);
+			//проходим по этому списку
+			foreach (string dir in DIR)
+			{	
+				//получаем список всех файлов в каждой из директории и
+				//предаем их в функцию сортировки
+				string[] lsFLS = Directory.GetFiles(dir);
+				foreach (string img in lsFLS)
+				{
+
+					sortIMG(img);
+				}	
+				//рекурсия от каждой директории(максимальное углубление )
+				GetDirectory(dir);
+
+				
+			}
+
+			return 0;
+		}
+		//2ой поток
+		private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+		{
+			//получаем список файлов в указаной директории с изображениями
+			Thread.CurrentThread.Priority = ThreadPriority.Highest;
+			string[] lsFLS = Directory.GetFiles(localFolder);
+			foreach (string img in lsFLS)
+			{
+				//сортируем их
+				sortIMG(img);
+			}
+			//если указано 'обрабатывать подпапки', тогда обрабатываем все папки которыем там есть
+			if (checkBox1.Checked == true)
+			{
+				GetDirectory(localFolder);
+			}
+
+			st.Stop();
+
+			ts = st.Elapsed;
+			//информируем пользователя
+			textBox3.Text = "Сортировка завершена!";
+			textBox3.Text += "\nВремя: \n" + ts.ToString();
+			checkBox1.Enabled = true;
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			//выбираем директорию с изображениями
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				textBox1.Text = folderBrowserDialog1.SelectedPath;
+			}
+		
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			//выбираем директорию с изображниями
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				textBox2.Text = folderBrowserDialog1.SelectedPath;
+				MessageBox.Show("Убедитесь, что директория для соритировки, не находиться в директории с сортируемыми изображениями.", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+	}
+}
